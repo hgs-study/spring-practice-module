39c080760215f67026c06ff1d03995e42da2b6f6
pir <hgstudy_@naver.com>
2022-12-23 23:15:11 +0900

    Initial commit

---
+++ aop/src/main/java/practice/pir/PirApplication.java
@@ -0,0 +1,13 @@
+package practice.pir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirApplication.class, args);
+	}
+
+}
