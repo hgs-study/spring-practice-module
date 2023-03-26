63525af5a00ffee75b6cf29866c4986e6c597d40
pir <hgstudy_@naver.com>
2023-03-27 00:00:58 +0900

    순수 JPA 기반 리포지토리

--- spring-data-jpa/src/main/java/practice/pir/repository/MemberJpaRepository.java
+++ spring-data-jpa/src/main/java/practice/pir/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import practice.pir.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -15,6 +17,25 @@ public class MemberJpaRepository {
     public Member save(Member member){
         entityManager.persist(member);
         return member;
+    }
+
+    public void delete(Member member){
+        entityManager.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return entityManager.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return entityManager.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id){
--- spring-data-jpa/src/test/java/practice/pir/repository/MemberJpaRepositoryTest.java
+++ spring-data-jpa/src/test/java/practice/pir/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import practice.pir.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +30,28 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(testUser.getId());
         assertThat(findMember.getUserName()).isEqualTo(testUser.getUserName());
         assertThat(findMember).isEqualTo(testUser);
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
