d03f9f43b50ebd4ffc74c94e0cd7ea8bcab0f836
pir <hgstudy_@naver.com>
2023-06-24 16:33:26 +0900

    [lecture] test : 카페 키오스크 테스트

---
+++ spring-lecture-cafekiosk-app/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,45 @@
+package sample.cafekiosk.unit;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CafeKioskTest {
+
+    @Test
+    void add(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+}
