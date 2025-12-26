8ff40464fdcee80483164ec1982eb421aab323e1
pir <hgstudy_@naver.com>
2025-12-26 18:04:19 +0900

    feat : create spring cloud stream kafka module

---
+++ spring-cloud-stream/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.9"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "spring-cloud-stream"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.3")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ spring-cloud-stream/src/main/kotlin/com/example/springcloudstream/SpringCloudStreamApplication.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.springcloudstream
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.stream.function.StreamBridge
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.stereotype.Component
+import java.io.IOException
+import java.io.UncheckedIOException
+import java.util.*
+import java.util.function.Consumer
+import java.util.function.Function
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+
+private val logger = KotlinLogging.logger {}
+
+@SpringBootApplication
+class KafkaBatchSampleApplication {
+    @KafkaListener(id = "batch-out", topics = ["batch-out"])
+    fun listen(`in`: String?) {
+        println(`in`)
+    }
+
+    @Bean
+    fun runner(template: KafkaTemplate<ByteArray?, ByteArray?>, environment: Environment): ApplicationRunner {
+        return ApplicationRunner { args: ApplicationArguments? -> IntStream.range(0, 10).forEach { i: Int -> template.send("batch-in",
+                if (environment.activeProfiles.contains("batch-produce"))
+                    "\"batch-test$i\"".toByteArray()
+                else
+                    "\"test$i\"".toByteArray()) }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplicationBuilder(KafkaBatchSampleApplication::class.java)
+//                    .profiles("default")
+                    .profiles("batch-produce")
+                    .run(*args)
+        }
+    }
+}
+
+open class Base {
+    @Autowired
+    val bridge: StreamBridge? = null
+}
+
+@Component
+@Profile("default")
+class NoTransactions : Base() {
+    @Bean
+    fun consumer(): Consumer<List<String>> {
+        return Consumer { list: List<String> -> list.forEach(Consumer { str: String ->
+            logger.info { "default consumer : $str"}
+            super.bridge?.send("output-out-0", str.uppercase(Locale.getDefault()))
+        }) }
+    }
+}
+
+@Component
+@Profile("transactional")
+class Transactions : Base() {
+    @Bean
+    fun consumer(): Consumer<List<String>> {
+        return Consumer { list: List<String> ->
+            list.forEach(Consumer { str: String -> super.bridge?.send("output-out-0", str.uppercase(Locale.getDefault())) })
+            println("Hit Enter to exit the listener and commit transaction")
+            try {
+                System.`in`.read()
+            } catch (e: IOException) {
+                throw UncheckedIOException(e)
+            }
+        }
+    }
+}
+
+@Component
+@Profile("batch-produce")
+class BatchProduce : Base() {
+    @Bean
+    fun consumer(): Function<List<String>, List<Message<String>>> {
+        return Function { list: List<String> ->
+            logger.info { "batch-produce consumer : $list"}
+            list.stream()
+                    .map { obj: String -> obj.uppercase(Locale.getDefault()) }
+                    .map { uppercasedString: String -> MessageBuilder.withPayload(uppercasedString).build() }
+                    .collect(Collectors.toList())
+        }
+    }
+}
